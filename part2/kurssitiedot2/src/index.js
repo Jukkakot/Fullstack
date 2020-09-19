f90b4812a69776fff65bbd1ef7e5291f2cf638e3
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-09-19 19:19:36 +0300

    2.2

--- part2/kurssitiedot2/src/index.js
+++ part2/kurssitiedot2/src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+const Header = (props) => {
+    return (
+        <h1>{props.course.name}</h1>
+    )
+}
+const Part = (props) => {
+    return (
+        <div>
+            <p>{props.part} {props.exercises}</p>
+        </div>
+    )
+}
+const Content = (props) => {
+    return (
+      <div>
+        {props.course.parts.map(part => 
+          <p key={part.id}>{part.name} {part.exercises}</p>
+        )}
+      </div>
+       
+    )
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Total = (props) => {
+  var partTotal = 0
+  for(var count of props.course.parts){
+    partTotal += count.exercises
+  }
+    return (
+        <div>
+        <p><b>Total of {partTotal} exercises</b></p>
+        </div>
+    )
+}
+const Course = (props) => {
+  return (
+    <div>
+            <Header course ={props.course}/>
+            <Content course ={props.course}/>
+            <Total course ={props.course}/>
+        </div>
+  )
+  //
+}
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'New example part',
+        exercises: 9,
+        id: 4
+      }
+    ]
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+    
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
