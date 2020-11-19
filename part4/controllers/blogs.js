c5c4cee9902e36cce46e77d6ec494016de9c2f53
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-11-19 15:05:58 +0200

    4.18 & 4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,32 +1,46 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('', async (request, response) => {
   //const blogs = await Blog.find({})
   const blogs = await Blog
-  .find({}).populate('user', { username: 1, name: 1 })
+    .find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const body = request.body
-  const user = await User.findById("5fad3faf54927a33305a553e")
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes === undefined ? 0 : body.likes,
-    user: user._id
-  })
-
   try {
+    const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes === undefined ? 0 : body.likes,
+      user: user._id
+    })
+
+
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     response.status(201).json(savedBlog.toJSON())
     await user.save()
-    
+
   } catch (exception) {
     next(exception)
   }
@@ -48,7 +62,7 @@ blogsRouter.put('/:id', async (request, response, next) => {
       error: 'content missing'
     })
   }
-  
+
 
   const blog = {
     title: body.title,
